d183d7036f1a52fbc277e2cfed5e68017a2b6d85
Roman Gergert <roman.gergert@bia-tech.ru>
2019-04-03 17:15:56 +0300

    second commit

--- app/src/main/java/ru/biatech/myapplication/MainActivity.kt
+++ app/src/main/java/ru/biatech/myapplication/MainActivity.kt
@@ -1,21 +1,34 @@
 package ru.biatech.myapplication
 
+import android.app.TabActivity
+import android.content.Intent
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.View
-import android.widget.ImageView
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+
     private var toolbar: Toolbar? = null
+    private var button: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initToolbar()
+
+        button = findViewById(R.id.bButton)
+        button?.setOnClickListener {
+            Toast.makeText(this, "Все доклады", Toast.LENGTH_LONG).show()
+        }
+
     }
+
 
     private fun initToolbar() {
         toolbar = findViewById(R.id.toolbar)
